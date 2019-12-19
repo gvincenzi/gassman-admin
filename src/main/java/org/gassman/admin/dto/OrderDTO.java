8e21d6ad5d48af166b3a6b91d827a32511994cde
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2019-12-19 15:12:19 +0100

    Total to pay computing in Order grid

--- src/main/java/org/gassman/admin/dto/OrderDTO.java
+++ src/main/java/org/gassman/admin/dto/OrderDTO.java
@@ -1,9 +1,9 @@
 package org.gassman.admin.dto;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
@@ -11,7 +11,13 @@ public class OrderDTO {
     private Long orderId;
     private UserDTO user;
     private Double quantity;
-    private BigDecimal totalToPay;
+    @Getter(AccessLevel.NONE)
+    private String totalToPay;
     private ProductDTO product;
     private Boolean payed = Boolean.FALSE;
+
+    public String getTotalToPay(){
+        return this.getQuantity() != null && this.getProduct() != null
+        && this.getProduct().getPricePerUnit() != null ? this.getQuantity() * (this.getProduct().getPricePerUnit()) + " €" : null;
+    }
 }
