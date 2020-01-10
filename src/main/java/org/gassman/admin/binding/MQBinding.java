eb45fa0174787dce46de89b9ff49da67a3f1bc4f
Giuseppe VINCENZI <gdvincenzi@airfrance.fr>
2020-01-10 13:09:51 +0100

    Users list push update

--- src/main/java/org/gassman/admin/binding/MQBinding.java
+++ src/main/java/org/gassman/admin/binding/MQBinding.java
@@ -5,7 +5,15 @@ import org.springframework.messaging.SubscribableChannel;
 
 public interface MQBinding {
     String USER_REGISTRATION = "userRegistrationChannel";
+    String ORDER_PAYMENT = "orderPaymentChannel";
+    String ORDER_PAYMENT_CONFIRMATION = "orderPaymentConfirmationChannel";
 
     @Input(USER_REGISTRATION)
     SubscribableChannel userRegistrationChannel();
+
+    @Input(ORDER_PAYMENT)
+    SubscribableChannel orderPaymentChannel();
+
+    @Input(ORDER_PAYMENT_CONFIRMATION)
+    SubscribableChannel userOrderPaymentConfirmationChannel();
 }
--- src/main/java/org/gassman/admin/listener/MQListener.java
+++ src/main/java/org/gassman/admin/listener/MQListener.java
@@ -2,6 +2,7 @@ package org.gassman.admin.listener;
 
 import com.vaadin.flow.component.UI;
 import org.gassman.admin.binding.MQBinding;
+import org.gassman.admin.dto.OrderDTO;
 import org.gassman.admin.dto.UserDTO;
 import org.gassman.admin.view.user.UsersView;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -21,4 +22,10 @@ public class MQListener {
     public void processUserRegistration(UserDTO msg) {
         ui.access(()->usersView.refreshUserGrid());
     }
+
+    @StreamListener(target = MQBinding.ORDER_PAYMENT_CONFIRMATION)
+    public void processOrderPaymentConfirmation(OrderDTO msg) {
+        ui.access(()->usersView.refreshUserGrid());
+    }
+
 }
